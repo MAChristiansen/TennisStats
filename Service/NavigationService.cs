c1ed496dfdee12c6fa3cb831d74619f7ef7325bd
Marcus August Christiansen <mac080497@gmail.com>
2019-03-04 16:53:53 +0100

    Add navigationService

---
+++ Service/NavigationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+
+namespace TennisStats.Service
+{
+    public class NavigationService
+    {
+        public static void NavigateToPage(Context context, Type activity) {
+
+            Intent intent = new Intent(context, activity);
+            context.StartActivity(intent);
+        }
+
+    }
+}
