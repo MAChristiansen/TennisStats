7eaeb998badacd003ca73d4600110569fb57908c
Marcus August Christiansen <mac080497@gmail.com>
2019-03-08 11:13:41 +0100

    Create FragmentScore

---
+++ FragmentScore.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace TennisStats
+{
+    public class FragmentScore : Fragment
+    {
+        public static FragmentScore NewInstance()
+        {
+            Bundle bundle = new Bundle();
+            
+            return new FragmentScore();
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            View view = inflater.Inflate(Resource.Layout.Score, container, false);
+
+
+
+            return view;
+        }
+    }
+}
