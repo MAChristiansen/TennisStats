b23875bb36e39b93bceea9c0a03ca0fe7fb7cebe
Marcus August Christiansen <mac080497@gmail.com>
2019-03-08 13:26:35 +0100

    Create Fragment StrokeType

--- FragmentStrokeType.cs
+++ FragmentStrokeType.cs
@@ -11,12 +11,15 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using TennisStats.Service;
 
 namespace TennisStats
 {
     public class FragmentStrokeType : Fragment
     {
 
+        private ImageView ivDropShot, ivSmash, ivVolley, ivLob, ivApproach, ivReturn, ivBaseline;
+        
         public static FragmentStrokeType NewInstance()
         {
             Bundle bundle = new Bundle();
@@ -35,10 +38,33 @@ namespace TennisStats
         {
             // Use this to return your custom view for this Fragment
             View view = inflater.Inflate(Resource.Layout.StrokeType, container, false);
-            
+
+            ivDropShot = view.FindViewById<ImageView>(Resource.Id.ivDropShot);
+            ivSmash = view.FindViewById<ImageView>(Resource.Id.ivSmash);
+            ivVolley = view.FindViewById<ImageView>(Resource.Id.ivVolley);
+            ivLob = view.FindViewById<ImageView>(Resource.Id.ivLob);
+            ivApproach = view.FindViewById<ImageView>(Resource.Id.ivApproach);
+            ivReturn = view.FindViewById<ImageView>(Resource.Id.ivReturn);
+            ivBaseline = view.FindViewById<ImageView>(Resource.Id.ivBaseline);
+
+            ivDropShot.Click += delegate { Navigate(); };
+            ivSmash.Click += delegate { Navigate();};
+            ivVolley.Click += delegate { Navigate(); };
+            ivLob.Click += delegate { Navigate(); };
+            ivApproach.Click += delegate { Navigate(); };
+            ivReturn.Click += delegate { Navigate();};
+            ivBaseline.Click += delegate { Navigate(); };
             
             
             return view;
+        }
+
+        private void Navigate()
+        {
+            NavigationService.NavigateToFragment(
+                FragmentManager,
+                Activity.FindViewById<FrameLayout>(Resource.Id.fragmentContainer),
+                FragmentWhichHand.NewInstance());
         }
     }
 }
