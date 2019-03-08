f97d2ca0ba578890201b79ed7ea1e5ca136d7eb7
Marcus August Christiansen <mac080497@gmail.com>
2019-03-08 13:08:35 +0100

    Create StrokeType Fragment

---
+++ FragmentStrokeType.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace TennisStats
+{
+    public class FragmentStrokeType : Fragment
+    {
+
+        public static FragmentStrokeType NewInstance()
+        {
+            Bundle bundle = new Bundle();
+            
+            return new FragmentStrokeType();
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            View view = inflater.Inflate(Resource.Layout.StrokeType, container, false);
+            
+            
+            
+            return view;
+        }
+    }
+}
