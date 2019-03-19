19016249409c0d2eaf7fa8809ee011572631f452
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-03-19 20:14:36 +0100

    Add button

--- ActivityLogin.cs
+++ ActivityLogin.cs
@@ -10,6 +10,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using TennisStats.Service;
 
 namespace TennisStats
 {
@@ -21,7 +22,11 @@ namespace TennisStats
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Login);
 
-            // Create your application here
+            Button btnCreateAccount = FindViewById<Button>(Resource.Id.btnCreateAccount);
+            Button btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+
+            btnLogin.Click += delegate { NavigationService.NavigateToPage(this, typeof(ActivityProfileSettings)); };
+            btnCreateAccount.Click += delegate { NavigationService.NavigateToPage(this, typeof(ActivityCreateAccount)); };
         }
     }
 }
