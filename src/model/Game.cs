df2e5bb4a5758f6a7d70df48cea87f3d046c53f6
Mathias Milter <mathiasmilter@gmail.com>
2019-03-20 16:03:10 +0100

    Serverid i et array istedet

--- src/model/Game.cs
+++ src/model/Game.cs
@@ -8,28 +8,29 @@ namespace TennisStats.Model
     {
 
         private string _winnerId;
-        private string _serverId;
         private GameType _gameType;
         private List<int> _team1Score;
         private List<int> _team2Score;
         private List<Point> _points;
 
+        private List<string> _servers;
+
         private Game(GameBuilder gb)
         {
             _winnerId = gb._winnerId;
-            _serverId = gb._serverId;
             _team1Score = gb._team1Score;
             _team2Score = gb._team2Score;
             _gameType = gb._gameType;
             _points = gb._points;
+            _servers = gb._servers;
         }
 
         public string WinnerId { get { return _winnerId; } set { _winnerId = value; } }
-        public string ServerId { get { return _serverId; } set { _serverId = value; } }
         public List<int> Team1Score { get { return _team1Score; } set { _team1Score = value; } }
         public List<int> Team2Score { get { return _team2Score; } set { _team2Score = value; } }
         public List<Point> Points { get { return _points; } set { _points = value; } }
         public GameType GameType { get { return _gameType; } set { _gameType = value; } }
+        public List<string> Servers { get { return _servers; } set { _servers = value; } }
 
         public int lastScoreTeam1 { get { if (_team1Score.Count > 0) { return _team1Score[_team1Score.Count - 1]; } else return 0; } }
         public int lastScoreTeam2 { get { if (_team2Score.Count > 0) { return _team2Score[_team2Score.Count - 1]; } else return 0; } }
@@ -39,7 +40,6 @@ namespace TennisStats.Model
         public class GameBuilder {
 
             public string _winnerId;
-            public string _serverId;
 
             public List<string> _servers;
 
@@ -51,7 +51,8 @@ namespace TennisStats.Model
 
             //Mandatory values
             public GameBuilder(string serverId) {
-                _serverId = serverId;
+                _servers = new List<string>();
+                _servers.Add(serverId);
                 _team1Score = new List<int>();
                 _team2Score = new List<int>();
                 _points = new List<Point>();
