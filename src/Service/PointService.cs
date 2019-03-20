f9be3901de1dbaefea72b82ac6f3dc7cef7a3a76
Marcus August Christiansen <mac080497@gmail.com>
2019-03-20 08:32:08 +0100

    Converting Points to Game Points

--- src/Service/PointService.cs
+++ src/Service/PointService.cs
@@ -1,10 +1,60 @@
 ﻿using System;
+using TennisStats.Enum;
+using TennisStats.Model;
+using static TennisStats.Enum.MatchTypeEnum;
+
 namespace TennisStats.src.Service
 {
     public class PointService
     {
         public PointService()
         {
+        }
+        
+        public string convertPoints(int pointToConvert, int opponent) {
+            if (pointToConvert < 4 && opponent < 4) {
+                switch (pointToConvert) {
+                    case 0:
+                        return "0";
+                    case 1:
+                        return "15";
+                    case 2:
+                        return "30";
+                    case 3:
+                        return "40";
+                }
+            }
+            else {
+                if (pointToConvert > opponent) {
+                    return "AD";
+                }
+                
+                return "40";
+                
+            }
+            return "";
+        }
+        
+        
+        //TODO: Do this method
+        public bool isGameDone(Game game, MatchType matchType)
+        {
+            
+            return false;
+        }
+
+        //TODO: Do this method
+        public bool isSetDone(Set set, MatchType matchType)
+        {
+            
+            return false;
+        }
+
+        //TODO: Do this method
+        public bool isMatchDone(Match match)
+        {
+
+            return false;
         }
     }
 }
