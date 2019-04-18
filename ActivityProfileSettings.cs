e06593a87f20889297ead960d6ed4d284b910838
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-04-18 10:48:41 +0200

    show datepicker

--- ActivityProfileSettings.cs
+++ ActivityProfileSettings.cs
@@ -14,14 +14,29 @@ using Android.Widget;
 namespace TennisStats
 {
     [Activity(Label = "ActivityProfileSettings")]
-    public class ActivityProfileSettings : Activity
+    public class ActivityProfileSettings : Activity, DatePickerDialog.IOnDateSetListener
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.ProfileSettings);
 
-            // Create your application here
+            Button btnBirthDay = FindViewById<Button>(Resource.Id.btnProfileSettingsBirthday);
+
+            btnBirthDay.Click += delegate { OnClickDatePicker(this); };
+
+        }
+        
+        private void OnClickDatePicker(Context context)
+        {
+            var dateTimeNow = DateTime.Now;
+            DatePickerDialog datePicker = new DatePickerDialog(context, this, dateTimeNow.Year, dateTimeNow.Month, dateTimeNow.Day);
+            datePicker.Show();
+        }
+
+        public void OnDateSet(DatePicker view, int year, int month, int dayOfMonth)
+        {
+            throw new NotImplementedException();
         }
     }
 }
