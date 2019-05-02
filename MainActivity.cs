3ab03122eba3c86610200fb4fcb409df4a944b03
Marcus August Christiansen <mac080497@gmail.com>
2019-05-02 14:41:54 +0200

    Testing

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,8 +1,14 @@
+using System;
+using System.Collections.Generic;
 using Android.App;
 using Android.Content;
 using Android.Widget;
 using Android.OS;
+using TennisStats.Model;
+using TennisStats.src.Controller;
 using TennisStats.Service;
+using static TennisStats.Enum.StatisticTypeEnum;
+using Point = System.Drawing.Point;
 
 namespace TennisStats
 {
@@ -18,16 +24,27 @@ namespace TennisStats
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.btnQuickMatch);
-            
+            Button btnQuickMatch = FindViewById<Button>(Resource.Id.btnQuickMatch);
+            Button btnCreateMatch = FindViewById<Button>(Resource.Id.btnCreateMatch);
             Button btnLogin = FindViewById<Button>(Resource.Id.btnLogIn);
 
-            button.Click += delegate
+            btnQuickMatch.Click += delegate
             {
                 NavigationService.NavigateToPage(this, typeof(ActivityMatchSetup));
             };
 
             btnLogin.Click += delegate { NavigationService.NavigateToPage(this, typeof(ActivityLogin)); };
+
+            btnCreateMatch.Click += async delegate
+            {
+                StatisticController sc = new StatisticController();
+
+                var listOfPoints = await sc.GetListOfPointsToBeCalculatedAsync("Frederikke", StatisticType.OVERALL);
+
+                double points = sc.calculateFirstServePercentage("Frederikke", listOfPoints);
+                
+                Console.WriteLine("Frederikkes først servs percent er: " + points);
+            };
         }
     }
 }
