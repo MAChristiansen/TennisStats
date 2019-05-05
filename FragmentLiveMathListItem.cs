ad4f73b7e4633c9a936b55745bc03156edbd1136
Steen Hansen <steen_home@hotmail.com>
2019-05-05 16:42:15 +0200

    Lavet et fragment til at agere som list item.

---
+++ FragmentLiveMathListItem.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using TennisStats.Model;
+
+namespace TennisStats
+{
+    public class FragmentLiveMathListItem : Fragment
+    {
+
+        private Match thisMatch;
+        
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.LiveMatchListItem, null);
+            return view;
+        }
+
+        public List<String> getMatchNames()
+        {
+            var strings = new List<String>();
+            strings.Add("Hans");
+            strings.Add("Erik");
+
+            return strings;
+        }
+        
+    }
+}
