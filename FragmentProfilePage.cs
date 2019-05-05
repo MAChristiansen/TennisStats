af6115a7acd6dc187e6eca2017298e947556399b
Steen Hansen <steen_home@hotmail.com>
2019-05-05 14:37:00 +0200

    Tilføjet createMatch knap til siden.

--- FragmentProfilePage.cs
+++ FragmentProfilePage.cs
@@ -11,6 +11,7 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using TennisStats.src.Controller;
 using TennisStats.Service;
 
 namespace TennisStats
@@ -18,13 +19,14 @@ namespace TennisStats
     public class FragmentProfilePage : Fragment
     {
         private TextView tvHeader;
-        private Button btnMyStats, btnLiveMatches, btnLogOut;
+        private Button btnMyStats, btnCreateMatch, btnLiveMatches, btnLogOut;
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             var view = inflater.Inflate(Resource.Layout.ProfilePage, container, false);
 
             btnMyStats = view.FindViewById<Button>(Resource.Id.btnMyStats);
+            btnCreateMatch = view.FindViewById<Button>(Resource.Id.btnCreateMatch);
             btnLiveMatches = view.FindViewById<Button>(Resource.Id.btnLiveMatches);
             btnLogOut = view.FindViewById<Button>(Resource.Id.btnLogOut);
 
@@ -52,6 +54,11 @@ namespace TennisStats
             {
                 // TODO : Some Logout code here, HANDLE IT JAAFAR!!
                 NavigationService.NavigateToPage(view.Context, typeof(MainActivity));
+            };
+            
+            btnCreateMatch.Click += async delegate
+            {
+                NavigationService.NavigateToPage(view.Context, typeof(ActivityMatchSetup));
             };
 
             return view;
