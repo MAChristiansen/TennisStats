f2d5358e8b50143cc6f496da791e573607325f6f
Steen Hansen <steen_home@hotmail.com>
2019-05-05 14:37:28 +0200

    Tilføjet preference manager funktioner til util

--- src/Controller/Util.cs
+++ src/Controller/Util.cs
@@ -1,11 +1,15 @@
 using System;
 using Android.App;
 using Android.Content;
+using Android.Preferences;
 
 namespace TennisStats.src.Controller
 {
     public class Util
     {
+
+        private static ISharedPreferences prefs;
+        
         public static AlertDialog.Builder SimpleAlert(Context context, string title, string message)
         {
             AlertDialog.Builder alert = new AlertDialog.Builder (context);
@@ -29,5 +33,19 @@ namespace TennisStats.src.Controller
             //DateTime.MinValue is 01/01/01 00:00 so add 1969 years. to get 1/1/1970
             return (long) DateTime.Now.Subtract(DateTime.MinValue.AddYears(1969)).TotalMilliseconds;           
         }
+
+        public static void logIn(Context context)
+        {
+            prefs = PreferenceManager.GetDefaultSharedPreferences(context);
+            ISharedPreferencesEditor editor = prefs.Edit();
+            editor.PutBoolean("userLogin", true);
+        }
+
+        public static Boolean isLoggetIn(Context context)
+        {
+            prefs = PreferenceManager.GetDefaultSharedPreferences(context);
+            return prefs.GetBoolean("userLogin", false);
+        }
+        
     }
 }
