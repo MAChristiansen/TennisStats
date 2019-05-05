199ae5a0417a17734a2d51652e3ea71e466d40ea
Steen Hansen <steen_home@hotmail.com>
2019-05-05 16:40:55 +0200

    Lavet en ny activitet til Live Matches

---
+++ ActivityLiveMatches.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using TennisStats.Service;
+
+namespace TennisStats
+{
+    [Activity(Label = "ActivityLiveMatches")]
+    public class ActivityLiveMatches : Activity
+    {
+        private ListView lvLiveMatches;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.LiveMatches); 
+            
+            lvLiveMatches = FindViewById<ListView>(Resource.Id.lvLiveMatches);
+
+            var mItems = new List<FragmentLiveMathListItem>();
+            mItems.Add(new FragmentLiveMathListItem());
+            
+            
+            lvLiveMatches.Adapter = new ListAdapter(this, mItems);
+           
+        }
+    }
+}
