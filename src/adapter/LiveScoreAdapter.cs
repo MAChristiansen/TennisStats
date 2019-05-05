eaa28f3f20ba1b7d10eb3f9dd6e354a31c3c9fe0
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-05-05 18:44:49 +0200

    add the rest

--- src/adapter/LiveScoreAdapter.cs
+++ src/adapter/LiveScoreAdapter.cs
@@ -27,11 +27,23 @@ namespace TennisStats.adapter
             TextView team1 = view.FindViewById<TextView>(Resource.Id.tvTeam1Name);
             TextView team2 = view.FindViewById<TextView>(Resource.Id.tvTeam2Name);
 
+            TextView set1 = view.FindViewById<TextView>(Resource.Id.tvTeam1Set);
+            TextView set2 = view.FindViewById<TextView>(Resource.Id.tvTeam2Set);
+
+            TextView game1 = view.FindViewById<TextView>(Resource.Id.tvTeam1Game);
+            TextView game2 = view.FindViewById<TextView>(Resource.Id.tvTeam2Game);
+
+            TextView point1 = view.FindViewById<TextView>(Resource.Id.tvTeam1Point);
+            TextView point2 = view.FindViewById<TextView>(Resource.Id.tvTeam2Point);
+
             Match match = GetItem(position);
             
             team1.Text = match.Team1Id;
             team2.Text = match.Team2Id;
 
+            set1.Text = match.Team1Score.ToString();
+            set2.Text = match.Team2Score.ToString();
+            
             return view;
         }
     }
