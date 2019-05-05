e0d756e5a40adb5f16e42ddbe9792a6cdfd9ada9
Steen Hansen <steen_home@hotmail.com>
2019-05-05 14:01:39 +0200

    Prøver at bygge en ListAdapter

---
+++ src/Service/AdapterService.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace TennisStats.src.Service
+{
+    public class AdapterService : ListFragment
+    {
+        public static void createAdapter(Context context)
+        {
+            var listAdapter = new ArrayAdapter<string>(context, Resource.Layout.Layout, countries);
+        }
+
+    }
+}
