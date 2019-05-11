10f961545340b109ff57ee3f8fbed6e67fea9d83
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-05-11 22:56:40 +0200

    Add activity and layout

---
+++ ActivityLiveScore.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Database;
+using TennisStats.adapter;
+using TennisStats.Model;
+using TennisStats.src.Controller;
+
+namespace TennisStats
+{
+    [Activity(Label = "ActivityLiveScore")]
+    public class ActivityLiveScore : Activity
+    {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.LiveScore);
+
+            List<Match> matchList = new List<Match>();
+            
+            ListView listView = FindViewById<ListView>(Resource.Id.livescore);
+
+            FirebaseClient firebaseClient = FBTables.FirebaseClient;
+            
+            var matches = await firebaseClient.Child(FBTables.FbMatch).OnceAsync<Match>();
+
+            foreach (var match in matches)
+            {
+                matchList.Add(match.Object);
+                Console.WriteLine("ADDED MATCH TO LIVESCORE");
+            }
+            
+            LiveScoreAdapter liveScoreAdapter = new LiveScoreAdapter(this, Resource.Layout.LiveScoreLayout, matchList);
+            listView.Adapter = liveScoreAdapter;
+
+//            List<string> list = new List<string>();
+//            list.Add("lol");
+//            list.Add("fuck");
+//            list.Add("haha");
+//            
+//            ArrayAdapter<string> arrayAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, list);
+//            listView.Adapter = arrayAdapter;
+
+        }
+    }
+}
